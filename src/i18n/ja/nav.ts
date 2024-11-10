d2748a7e54898bcb68ad0f3d58e5703f9fcd8e9b
Wuzi <wuzi@obiscr.com>
2024-11-10 14:13:16 +0800

    added: Add Japanese template

---
+++ src/i18n/ja/nav.ts
@@ -0,0 +1,27 @@
+import { NavDictionary } from '../translation-checkers';
+
+export default NavDictionary({
+	welcome: '欢迎',
+	'about-xiangqian-zhang': '关于张祥前',
+	'videos-of-zhang': '张祥前的视频',
+	'donate-zhang': '捐赠张祥前',
+	'about-website': '关于本网站',
+	'join-community': '加入社区',
+	researchExperiment: '研究实验',
+	'research-experiment/generation-of-gravitational-fields-by-low-voltage-varying-electromagnetic-fields': '低电压变化电磁场产生引力场试验',
+	academicPaper: '学术论文',
+	'academic-paper/unified-field-equation-generated-by-longitudinal-electromagnetic-lem-waves': '纵向电磁波 (LEM) 产生的统一场方程',
+	resources: '资源',
+	'resources/relevant-literature': '相关文献',
+	'resources/field-animations': '宇宙四大场动画演示',
+	'resources/use-of-artificial-sites': '人工场的应用场景',
+	unifiedFieldTheory: '统一场论',
+	'unified-field-theory/chapter-1': '第一章',
+	'unified-field-theory/chapter-2': '第二章',
+	'unified-field-theory/chapter-3': '第三章',
+	'unified-field-theory/chapter-4': '第四章',
+	'unified-field-theory/chapter-5': '第五章',
+	'unified-field-theory/chapter-6': '第六章',
+	'unified-field-theory/chapter-7': '第七章',
+	'unified-field-theory/chapter-8': '第八章',
+});
--- src/i18n/languages.ts
+++ src/i18n/languages.ts
@@ -1,6 +1,7 @@
 export const allLanguages = {
 	en: 'English',
 	'zh-cn': '简体中文',
+	ja: '日本語',
 } as const;
 
 /**
