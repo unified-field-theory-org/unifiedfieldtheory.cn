b6cfcf542de92758ffda2a8f2a88eea3996e36d7
bzsgbq <81854989+bzsgbq@users.noreply.github.com>
2024-11-26 13:19:52 +0800

    Initial update section 31
    Signed-off-by: bzsgbq <81854989+bzsgbq@users.noreply.github.com>

--- src/data/discussions.ts
+++ src/data/discussions.ts
@@ -70,6 +70,8 @@ export const forumLinks: LinkMap = {
 		'https://forum.unifiedfieldtheory.cn/questions/10010000000000162',
 	'/main-works/unified-field-theory/section-30':
 		'https://forum.unifiedfieldtheory.cn/questions/10010000000000163',
+	'/main-works/unified-field-theory/section-31':
+		'https://forum.unifiedfieldtheory.cn/questions/10010000000000164',
 } as const; // 使用 as const 使值变为只读
 
 // 可选：导出一个获取链接的工具函数
